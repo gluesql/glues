18ead6f846df88ecb66cd76a00f7ce7b1c228fbc
Taehoon Moon <taehoon.moon@outlook.com>
2024-05-28 16:00:34 +0900

    Add CursiveExt trait for providing syntax sugar to tui

---
+++ tui/src/cursive_ext.rs
@@ -0,0 +1,48 @@
+use {
+    crate::components::{alert::render_alert, confirm::render_confirm},
+    cursive::{
+        Cursive,
+        {view::View, views::ViewRef},
+    },
+    glues_core::Glues,
+};
+
+pub trait CursiveExt {
+    fn glues(&mut self) -> &mut Glues;
+
+    fn find<V: View>(&mut self, id: &str) -> ViewRef<V>;
+
+    fn confirm<F>(&mut self, message: String, on_confirm: F)
+    where
+        F: Fn(&mut Cursive) + Send + 'static;
+
+    fn alert<F>(&mut self, message: String, on_close: F)
+    where
+        F: Fn(&mut Cursive) + Send + 'static;
+}
+
+impl CursiveExt for Cursive {
+    fn glues(&mut self) -> &mut Glues {
+        self.user_data::<Glues>().expect("Glues must exist")
+    }
+
+    fn find<V: View>(&mut self, id: &str) -> ViewRef<V> {
+        self.find_name(id).expect("View with {id} must exist")
+    }
+
+    fn confirm<F>(&mut self, message: String, on_confirm: F)
+    where
+        F: Fn(&mut Cursive) + Send + 'static,
+    {
+        let dialog = render_confirm(&message, on_confirm);
+        self.add_layer(dialog);
+    }
+
+    fn alert<F>(&mut self, message: String, on_close: F)
+    where
+        F: Fn(&mut Cursive) + Send + 'static,
+    {
+        let dialog = render_alert(&message, on_close);
+        self.add_layer(dialog);
+    }
+}
