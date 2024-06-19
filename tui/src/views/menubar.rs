d60173859a2bd854c7399741038e999c2b7b3011
Taehoon Moon <taehoon.moon@outlook.com>
2024-06-19 16:19:44 +0900

    Update menubar label text to disabled

--- tui/src/views/menubar.rs
+++ tui/src/views/menubar.rs
@@ -1,10 +1,10 @@
-use cursive::{event::Key, Cursive};
+use cursive::{event::Key, menu::Item as MenuItem, Cursive};
 
 pub fn menubar(siv: &mut Cursive) {
     siv.menubar()
         .add_leaf("Quit", |s| s.quit())
         .add_delimiter()
-        .add_leaf("[ESC] to focus", |_| {});
+        .item(MenuItem::leaf("[ESC] to focus", |_| {}).disabled());
 
     siv.add_global_callback(Key::Esc, |s| s.select_menubar());
     siv.set_autohide_menu(false);
