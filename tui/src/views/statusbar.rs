9b5f6e73f4caaeec3b721f2c591f7b1f39a3d34f
Taehoon Moon <taehoon.moon@outlook.com>
2024-07-09 14:34:56 +0900

    Change statusbar layout - desc to the right and actions to the left

--- tui/src/views/statusbar.rs
+++ tui/src/views/statusbar.rs
@@ -11,17 +11,17 @@ pub fn render_statusbar(siv: &mut Cursive) -> impl View {
     let statusbar_node = Node::statusbar();
 
     let description = siv.glues().state.describe();
-    let description = TextView::new(description)
-        .with_name(statusbar_node.description().name())
-        .full_width();
+    let description = TextView::new(description).with_name(statusbar_node.description().name());
 
     let shortcuts = siv.glues().state.shortcuts().join(", ");
-    let shortcuts = TextView::new(shortcuts).with_name(statusbar_node.shortcuts().name());
+    let shortcuts = TextView::new(shortcuts)
+        .with_name(statusbar_node.shortcuts().name())
+        .full_width();
 
     let layout = LinearLayout::horizontal()
-        .child(description)
-        .child(shortcuts);
-    let padded_view = PaddedView::lrtb(2, 2, 0, 0, layout);
+        .child(shortcuts)
+        .child(description);
+    let padded_view = PaddedView::lrtb(1, 2, 0, 0, layout);
 
     Layer::new(padded_view).full_width()
 }
