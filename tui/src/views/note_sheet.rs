629cb4d12c2360c57b45d9ec7fb0958ce205da40
Taehoon Moon <taehoon.moon@outlook.com>
2024-07-31 12:08:38 +0900

    Remove CircularFocus in NoteSheet view

--- tui/src/views/note_sheet.rs
+++ tui/src/views/note_sheet.rs
@@ -1,15 +1,10 @@
 use {
     crate::views::{editor::render_editor, note_tree::render_note_tree},
-    cursive::{
-        view::View,
-        views::{CircularFocus, LinearLayout},
-        Cursive, With,
-    },
+    cursive::{view::View, views::LinearLayout, Cursive},
 };
 
 pub fn render_note_sheet(siv: &mut Cursive) -> impl View {
     LinearLayout::horizontal()
         .child(render_note_tree(siv))
         .child(render_editor(siv))
-        .wrap_with(CircularFocus::new)
 }
