70298f6f1aa9aa6c36cf663ad74339963f0c4075
Taehoon Moon <taehoon.moon@outlook.com>
2024-08-20 21:43:22 +0900

    Fix TUI add_directory transition to update children properly

--- tui/src/transitions/notebook/add_directory.rs
+++ tui/src/transitions/notebook/add_directory.rs
@@ -1,33 +1,17 @@
 use {
-    crate::{traits::*, views::notebook::note_tree::directory::render_directory, Node},
+    crate::{traits::*, Node},
     cursive::Cursive,
-    glues_core::{
-        data::Directory,
-        state::notebook::{DirectoryItem, NotebookState},
-        NotebookEvent,
-    },
+    glues_core::{data::Directory, NotebookEvent},
 };
 
 pub fn add_directory(siv: &mut Cursive, directory: Directory) {
-    if !siv
-        .state::<NotebookState>()
-        .check_opened(&directory.parent_id)
-    {
-        siv.dispatch(NotebookEvent::OpenDirectory(directory.parent_id.clone()));
-    } else {
-        let mut container = Node::notebook()
-            .note_tree()
-            .directory(&directory.parent_id)
-            .note_list()
-            .find(siv);
+    Node::notebook()
+        .note_tree()
+        .directory(&directory.parent_id)
+        .find(siv)
+        .remove_child(1);
 
-        let directory_item = DirectoryItem {
-            directory: directory.clone(),
-            children: None,
-        };
-        container.add_child(render_directory(siv, directory_item));
-    }
-
+    siv.dispatch(NotebookEvent::OpenDirectory(directory.parent_id.clone()));
     siv.focus_on_next_tick(
         Node::notebook()
             .note_tree()
