884ebe54d3a8f030e96f8711a4424f3c42c1ad33
Taehoon Moon <taehoon.moon@outlook.com>
2024-08-26 16:27:03 +0900

    Make tui note tree view scrollable

--- tui/src/views/notebook/note_tree.rs
+++ tui/src/views/notebook/note_tree.rs
@@ -4,7 +4,7 @@ pub mod note;
 use {
     crate::{node::Node, traits::*},
     cursive::{
-        view::{Nameable, Resizable},
+        view::{Nameable, Resizable, Scrollable},
         views::{PaddedView, Panel},
         Cursive, View,
     },
@@ -17,6 +17,7 @@ pub fn render_note_tree(siv: &mut Cursive) -> impl View {
     let directory_item = root.clone();
     let layout = render_directory(siv, directory_item)
         .with_name(Node::notebook().note_tree().note_list().name())
+        .scrollable()
         .min_width(40);
     let padded_view = PaddedView::lrtb(1, 1, 0, 1, layout);
 
