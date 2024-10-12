b2cb132265e5de16f350123e78ac53fe195ee13a
Taehoon Moon <taehoon.moon@outlook.com>
2024-10-12 15:19:58 +0900

    Add big text title 'Glues' to entry view

--- ratatui/src/views/body/entry.rs
+++ ratatui/src/views/body/entry.rs
@@ -3,21 +3,27 @@ use {
     glues_core::state::EntryState,
     ratatui::{
         layout::{Alignment, Constraint::Length, Flex, Layout, Rect},
-        style::{Color, Style},
+        style::{Color, Style, Stylize},
         widgets::{Block, HighlightSpacing, List, ListDirection, Padding},
         Frame,
     },
+    tui_big_text::BigText,
 };
 
 pub fn draw(frame: &mut Frame, area: Rect, _state: &EntryState, context: &mut EntryContext) {
-    let [area] = Layout::horizontal([Length(24)])
+    let [area] = Layout::horizontal([Length(38)])
         .flex(Flex::Center)
         .areas(area);
-    let [area] = Layout::vertical([Length(9)]).flex(Flex::Center).areas(area);
+    let [title_area, area] = Layout::vertical([Length(9), Length(9)])
+        .flex(Flex::Center)
+        .areas(area);
 
+    let title = BigText::builder()
+        .lines(vec!["Glues".dark_gray().into()])
+        .build();
     let block = Block::bordered()
         .padding(Padding::new(2, 2, 1, 1))
-        .title("[Glues] Open Notes")
+        .title("Open Notes")
         .title_alignment(Alignment::Center);
 
     let items = ["Instant", "CSV", "JSON", "File", "Git"];
@@ -28,5 +34,6 @@ pub fn draw(frame: &mut Frame, area: Rect, _state: &EntryState, context: &mut En
         .highlight_spacing(HighlightSpacing::Always)
         .direction(ListDirection::TopToBottom);
 
+    frame.render_widget(title, title_area);
     frame.render_stateful_widget(list, area, &mut context.list_state);
 }
