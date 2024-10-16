014447a79f7701cf4931087a6d88b8704a1556e5
Taehoon Moon <taehoon.moon@outlook.com>
2024-10-17 00:11:25 +0900

    Update statusbar design

--- ratatui/src/views/statusbar.rs
+++ ratatui/src/views/statusbar.rs
@@ -2,23 +2,36 @@ use {
     crate::logger::*,
     glues_core::state::State,
     ratatui::{
-        layout::{Constraint::Min, Flex, Layout, Rect},
+        layout::{
+            Constraint::{Min, Percentage},
+            Flex, Layout, Rect,
+        },
         style::Stylize,
-        text::Text,
+        text::{Line, Text},
         Frame,
     },
 };
 
 pub fn draw(frame: &mut Frame, area: Rect, state: &State) {
-    let shortcuts = state.shortcuts().join(", ");
+    let mut shortcuts = vec![" ".into()];
+
+    for (i, shortcut) in state.shortcuts().iter().enumerate() {
+        shortcuts.push(shortcut.black());
+
+        if i < state.shortcuts().len() - 1 {
+            shortcuts.push(" | ".light_blue().on_gray());
+        }
+    }
+
+    let shortcuts: Line = shortcuts.into();
     let description = state.describe().log_unwrap() + " ";
 
     let [shortcuts_area, description_area] =
-        Layout::horizontal([Min(shortcuts.len() as u16), Min(description.len() as u16)])
+        Layout::horizontal([Percentage(100), Min(description.len() as u16)])
             .flex(Flex::SpaceBetween)
             .areas(area);
 
-    frame.render_widget(Text::raw(shortcuts).black().on_gray(), shortcuts_area);
+    frame.render_widget(shortcuts.on_gray(), shortcuts_area);
     frame.render_widget(
         Text::raw(description).right_aligned().black().on_gray(),
         description_area,
