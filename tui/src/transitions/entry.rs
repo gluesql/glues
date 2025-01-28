ea7a16a712f7a996b176abc54d4f83d63a4b9fdc
Taehoon Moon <taehoon.moon@outlook.com>
2025-01-28 13:19:17 +0900

    Split tui/transitions.rs into sub modules

---
+++ tui/src/transitions/entry.rs
@@ -0,0 +1,25 @@
+use {
+    crate::{context::ContextState, logger::*, App},
+    glues_core::{
+        state::{GetInner, NotebookState},
+        transition::EntryTransition,
+    },
+};
+
+impl App {
+    pub(super) async fn handle_entry_transition(&mut self, transition: EntryTransition) {
+        match transition {
+            EntryTransition::OpenNotebook => {
+                log!("Opening notebook");
+
+                let NotebookState { root, .. } = self.glues.state.get_inner().log_unwrap();
+                self.context.state = ContextState::Notebook;
+                self.context.notebook.update_items(root);
+            }
+            EntryTransition::Inedible(event) => {
+                log!("Inedible event: {event}");
+            }
+            EntryTransition::None => {}
+        }
+    }
+}
